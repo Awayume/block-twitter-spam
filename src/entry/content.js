fef0c3f442084df2d3a8514376e94d0c6ec804af
Awayume <dev@awayume.jp>
2024-03-21 18:09:58 +0900

    ファイル名を変更

--- src/entry/content.js
+++ src/entry/content.js
@@ -1,6 +1,6 @@
 (() => {
   const script = document.createElement('script');
-  script.setAttribute('src', chrome.runtime.getURL('./script/script.js'));
+  script.setAttribute('src', chrome.runtime.getURL('/src/main.js'));
   script.setAttribute('type', 'module');
   script.addEventListener('load', () => {
     console.log('loaded');
