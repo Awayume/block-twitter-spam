4f1d39f2643f9a684c9026ea9d38f9733bb897fc
Awayume <dev@awayume.jp>
2024-03-24 19:29:31 +0900

    スパム語録の確認に正規表現を使用

--- src/checker/word.js
+++ src/checker/word.js
@@ -5,11 +5,22 @@
  * @return {boolean} スパムによくある文言が含まれていた場合はtrue, そうでない場合はfalse
  */
 export const checkSpamWords = (text) => {
-  /** @type {Array<string>} スパムによくある文言を入れる配列 */
-  const spamWords = ['お前のプロフ抜けるわ', 'よかったらプロフ見て'];
+  /** @type {Array<RegExp>} スパムによくある文言の正規表現の配列 */
+  const spamWords = [
+    /お前の(プロフ|固ツイ|固定(ツイート|ポスト)?).*(抜|ヌ)ける(わ)?/,
+    /(よ|良)かったら(プロフ|ぷろふ)(見|み)て/,
+  ];
+  // eslint-disable-next-line sonarjs/no-unused-collection
+  const results = [];
   let isSpam = false;
   for (const spamWord of spamWords) {
-    if (text.includes(spamWord)) isSpam = true;
+    const result = text.match(spamWord);
+    if (result) {
+      results.push(result);
+      isSpam = true;
+      break;
+    }
   }
+  // TODO: resultsを使ってログ処理
   return isSpam;
 };
