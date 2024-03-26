7a8e6ae1e4031b3fd709373546803ce13314b2e6
Awayume <dev@awayume.jp>
2024-03-26 18:35:49 +0900

    forを削減して言語判定処理を効率化

--- src/utils/language.js
+++ src/utils/language.js
@@ -11,20 +11,20 @@
 export const characterRanges = {
   // NOTE: 間違いが複数あり
   // TODO: 正しいものに修正する
-  ja: /[\u3040-\u30FF]|[\uFF65-\uFF9F]|[\u4E00-\u9FFF]/, // ひらがな,カタカナ|半角カタカナ|漢字
+  ja: /[\u3040-\u30FF]|[\uFF65-\uFF9F]|[\u4E00-\u9FFF]/g, // ひらがな,カタカナ|半角カタカナ|漢字
   // eslint-disable-next-line unicorn/better-regex
-  en: /[\u0041-\u005A]|[\u0061-\u007A]/, // 英大文字|英小文字
-  ar: /[\u0600-\u06FF]/, // アラビア文字
-  zh: /[\u4E00-\u9FFF]/, // 漢字（中国語）
-  ko: /[\uAC00-\uD7AF]/, // ハングル（韓国語）
-  ru: /[\u0400-\u04FF]/, // キリル文字（ロシア語）
-  he: /[\u0590-\u05FF]/, // ヘブライ文字
-  hi: /[\u0900-\u097F]/, // ヒンディー語
-  th: /[\u0E00-\u0E7F]/, // タイ文字
-  fa: /[\u0600-\u06FF]/, // ペルシャ語
-  de: /[\u00C0-\u00FF]|\u0152|\u0153|[\u20A0-\u20CF]/, // Umlauts,ß, etc.|Œ,œ|Currency symbols, etc.
-  es: /[\u00C0-\u00FF]|[\u20A0-\u20CF]/, // Accented characters|Currency symbols, etc.
-  emoji: /[\uD83C\uDC00-\uD83E\uDDFF]/u, // 絵文字
+  en: /[\u0041-\u005A]|[\u0061-\u007A]/g, // 英大文字|英小文字
+  ar: /[\u0600-\u06FF]/g, // アラビア文字
+  zh: /[\u4E00-\u9FFF]/g, // 漢字（中国語）
+  ko: /[\uAC00-\uD7AF]/g, // ハングル（韓国語）
+  ru: /[\u0400-\u04FF]/g, // キリル文字（ロシア語）
+  he: /[\u0590-\u05FF]/g, // ヘブライ文字
+  hi: /[\u0900-\u097F]/g, // ヒンディー語
+  th: /[\u0E00-\u0E7F]/g, // タイ文字
+  fa: /[\u0600-\u06FF]/g, // ペルシャ語
+  de: /[\u00C0-\u00FF]|\u0152|\u0153|[\u20A0-\u20CF]/g, // Umlauts,ß, etc.|Œ,œ|Currency symbols, etc.
+  es: /[\u00C0-\u00FF]|[\u20A0-\u20CF]/g, // Accented characters|Currency symbols, etc.
+  emoji: /[\uD83C\uDC00-\uD83E\uDDFF]/gu, // 絵文字
 };
 
 /**
@@ -33,38 +33,28 @@ export const characterRanges = {
  * @param {string} text - ツイートのテキスト
  * @return {LangInfo} 推測したツイートの主言語
  */
-export const detectLang = (text) => { // eslint-disable-line sonarjs/cognitive-complexity
-  /** @type {Object<string, number>} */
-  const langStats = {};
-  for (const lang in characterRanges) langStats[lang] = 0;
-  langStats['unknown'] = 0;
+export const detectLang = (text) => {
+  /** @type {Array<{lang: string, ratio: number}>} */
+  const langStats = [];
+  let textCount = text.length;
 
-  // 言語ごとの出現数をカウント
-  for (const char of text) {
-    let isFound = false;
-
-    for (const lang in characterRanges) {
-      if (characterRanges[lang].test(char)) {
-        langStats[lang]++;
-        isFound = true;
-        break;
-      }
+  // 言語ごとの出現数をカウントし割合を求める
+  for (const lang in characterRanges) {
+    const result = text.match(characterRanges[lang]);
+    if (result) {
+      langStats.push({lang: lang, ratio: result.length / text.length});
+      textCount = textCount - result.length;
+      if (!textCount) break;
     }
-
-    if (!isFound) langStats['unknown']++;
   }
 
-  // 言語ごとに割合を求める
-  /** @type {Array<{lang: string, ratios}>} */
-  const langRatios = [];
-  for (const lang in langStats) {
-    langRatios.push({lang: lang, percentage: langStats[lang] / text.length});
-  }
+  // どの言語にも分類されていない文字をunknownとして扱う
+  langStats['unknown'] = textCount;
 
   // 割合の高い順にソート
-  langRatios.sort((a, b) => {
-    return b.percentage - a.percentage;
+  langStats.sort((a, b) => {
+    return b.ratio - a.ratio;
   });
 
-  return {primary: langRatios[0].lang, secondary: langRatios[1] ? langRatios[1].lang : null};
+  return {primary: langStats[0].lang, secondary: langStats[1]?.ratio ? langStats[1].lang : null};
 };
