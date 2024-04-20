8248a50510449ed9fd15e503c0dbafb5d145a35b
Awayume <dev@awayume.jp>
2024-04-21 01:23:55 +0900

    フィールド構文を使用

--- src/checker/base.js
+++ src/checker/base.js
@@ -1,27 +1,34 @@
 /**
+ * @typedef {Object} CheckResult - スパムチェックの結果
+ * @property {number} score - スパムスコア
+ * @property {Array<string>} reasons - 判定の理由
+ */
+
+/**
  * 各チェッカーの基底クラス。
  */
 export default class CheckerBase {
+  /** @type {import('../twitter/tweet.js').Tweet} スパムチェックに使用するデータ */
+  #data = undefined;
+  /** @type {Object<string, any>} チェッカーのオプション */
+  #options = undefined;
+
   /**
    * クラスを初期化する。
    *
-   * @template T
-   * @template U
-   * @param {T} data - スパムチェックに用いるデータ
-   * @param {Object<string, U>} [options] - チェッカーのオプション
+   * @param {import('../twitter/tweet.js').Tweet} data - スパムチェックに使用するデータ
+   * @param {Object<string, any>} [options] - チェッカーのオプション
    */
   constructor(data, options) {
-    /** @type {T} */
-    this.data = data;
-    /** @type {Object<string, U>} */
-    this.options = options;
+    this.#data = data;
+    this.#options = options;
   }
 
   /**
    * スパムチェックを行う。
    *
    * @async
-   * @return {Promise<{score: number, reasons: Array<string>}>} スパムチェックの結果
+   * @return {Promise<CheckResult>} スパムチェックの結果
    */
   async check() {
     throw new TypeError('TypeError: このメソッドはオーバーライドされる必要があります');
